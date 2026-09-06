595a55f77fd3e969576be563816311bedce21097
Mykola Hohsadze <koliagogsadze@gmail.com>
2026-09-06 20:37:21 +0300

    Remove bootconfig androidboot.boot_devices
    Configuration androidboot.boot_devices moved to bootloader stage

--- qemu_arm64/BoardConfig.mk
+++ qemu_arm64/BoardConfig.mk
@@ -5,9 +5,3 @@ TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := arm64-v8a
 
 -include device/generic/qemu/shared/BoardConfig.mk
-
-#
-# Bootconfig
-#
-
-BOARD_BOOTCONFIG += androidboot.boot_devices=a003c00.virtio_mmio
