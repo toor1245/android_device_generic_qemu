c670b41ff41af0ec60752b2a85e8fbf45a024cfb
Mykola Hohsadze <koliagogsadze@gmail.com>
2026-09-06 20:43:19 +0300

    Add recovery support

--- shared/device.mk
+++ shared/device.mk
@@ -1,7 +1,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_no_zygote.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+PRODUCT_VIRTUAL_AB_COW_VERSION := 3
+PRODUCT_VIRTUAL_AB_COMPRESSION_FACTOR := 65536
 
 PRODUCT_COPY_FILES += \
     device/generic/qemu/shared/fstab.qemu:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qemu \
@@ -10,6 +14,8 @@ PRODUCT_COPY_FILES += \
     device/generic/qemu/shared/fstab.qemu:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.qemu \
     device/generic/qemu/shared/fstab.qemu:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qemu \
     device/generic/qemu/shared/init.qemu.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qemu.rc \
+
+TARGET_RECOVERY_FSTAB := device/generic/qemu/shared/fstab.qemu
 
 SKIP_BOOT_JARS_CHECK ?= true
 
@@ -63,8 +69,7 @@ PRODUCT_PACKAGES += \
     sanitizer.libraries.txt \
     vdc \
     vold \
-    update_engine \
-    bootctl \
+    lshal \
     linker.vendor_ramdisk \
 
 PRODUCT_PACKAGES += \
@@ -74,6 +79,33 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.runtime \
     com.android.adbd \
+    com.android.tethering \
+
+# Recovery packages
+PRODUCT_PACKAGES += \
+    recovery \
+    init_second_stage.recovery \
+    ld.config.recovery.txt \
+    linker.recovery \
+    shell_and_utilities_recovery \
+    adbd.recovery \
+    servicemanager.recovery \
+    cgroups.recovery.json \
+    otacerts.recovery \
+    watchdogd.recovery \
+    resize2fs.vendor_ramdisk \
+    tune2fs.vendor_ramdisk \
+
+PRODUCT_COPY_FILES += \
+    device/generic/qemu/shared/init.recovery.qemu.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qemu.rc
+
+# OTA packages
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    bootctl \
+    com.android.hardware.boot \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
